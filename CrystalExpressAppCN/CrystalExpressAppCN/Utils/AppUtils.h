a238d039c1a9c56a51a25a1abdf73bdbdd9e7945
roylo <joeblack1986@gmail.com>
2015-08-25 12:29:55 +0800

    add missing files

---
+++ CrystalExpressAppCN/CrystalExpressAppCN/Utils/AppUtils.h
@@ -0,0 +1,14 @@
+//
+//  AppUtils.h
+//  crystalexpress
+//
+//  Created by roylo on 2015/4/9.
+//  Copyright (c) 2015年 intowow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppUtils : NSObject
++ (NSString *)getAppVersion;
++ (NSString *)decidePlacementName:(NSString *)curPlacementName;
+@end
